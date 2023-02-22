124f8b8ee1523c31340f43d747570d3f8a2fd199
Transformers.Sh <sheruya.shosafoev@tekschool.us>
2023-02-21 18:49:15 -0800

    this

--- src/package1/GitPracticeTwo.java
+++ src/package1/GitPracticeTwo.java
@@ -1,8 +1,9 @@
 package package1;
 
 public class GitPracticeTwo {
-public static void main(String[] args) {
-	System.out.println("This is for Push practice");
-	System.out.println("This is pull practice");
-}
+	public static void main(String[] args) {
+		System.out.println("This is for Push practice");
+		System.out.println("This is pull practice");
+		System.out.println("This is for revert practice");
+	}
 }
