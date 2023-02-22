33648dd5f2a013cb069e843ec258376ea92ad6d6
wikiname-o4 <125518680+wikiname-o4@users.noreply.github.com>
2023-02-21 17:02:16 -0800

    Update GitPracticeTwo.java
    This is for pull practice

--- src/package1/GitPracticeTwo.java
+++ src/package1/GitPracticeTwo.java
@@ -3,5 +3,6 @@ package package1;
 public class GitPracticeTwo {
 public static void main(String[] args) {
 	System.out.println("This is for Push practice");
+	System.out.println("This is pull practice");
 }
 }
