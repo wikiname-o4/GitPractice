52ab506a33907c10be458f2c748127062ef404e0
wikiname-o4 <125518680+wikiname-o4@users.noreply.github.com>
2023-02-21 18:19:20 -0800

    Update TestBranch.java

--- src/package1/TestBranch.java
+++ src/package1/TestBranch.java
@@ -4,4 +4,8 @@ public class TestBranch {
 public static void testOne() {
 	System.out.println("This is for branch purpose");
 }
+	
+	public static void testTwo() {
+	System.out.println("This is for branch purpose");
+}
 }
